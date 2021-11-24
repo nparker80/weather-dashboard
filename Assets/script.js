a2a1eed45b279c22c28e62febafd63a536d18529
Najla Parker <najla08@gmail.com>
2021-11-24 15:44:07 -0700

    Revised index.html to update overall layout and added the submitForm function to script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,24 @@
-const APIKey = "fca898804db55b003fe45d3544a59034";
+let cities = [];
 
-let city;
-let queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&appid=" +
-  APIKey;
+const citySearchForm = document.querySelector("#input-form");
+const cityInput = document.querySelector("#city");
+const currentWeatherContainer = document.querySelector("#search-container");
+const searchedCity = document.querySelector("#searched-city");
+const forecast = document.querySelector("#forecast");
+const fiveDayContainer = document.querySelector("#five-day-container");
+const previousCitySearch = document.querySelector("#previous-city-search-button");
 
-fetch(queryURL);
+let formSubmit=function(event) {
+  event.preventDefault();
+  let city=cityInput.value.trim();
+
+  if(city) {
+    getCityWeather(city);
+    getFiveDayForecast(city);
+    cities.unshift({city});
+    cityInput.value="";
+  } else{
+    alert("Please Enter a City Name");
+}
+
+citySearchForm.addEventListener("submit", formSubmit);
